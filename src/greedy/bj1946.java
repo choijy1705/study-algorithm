384a5c939d4632f5fa3655b5eeb81fbe4ba9b4f8
Junyoung Choi <cjy1705@naver.com>
2020-10-11 22:56:23 +0900

    greedy

--- src/greedy/bj1946.java
+++ src/greedy/bj1946.java
@@ -8,32 +8,18 @@ public class bj1946 {
 
         for(int tc=0;tc<t;tc++){
             n = sc.nextInt();
-
-            int[][] student = new int[n][2];
+            int[] score = new int[n+1];
 
             for(int i=0;i<n;i++){
-                student[i][0] = sc.nextInt();
-                student[i][1] = sc.nextInt();
-
+                score[sc.nextInt()] = sc.nextInt();
             }
-
-            Arrays.sort(student, new Comparator<int[]>(){
-
-                @Override
-                public int compare(int[] o1, int[] o2) {
-                    return o1[0] - o2[0];
+            int min = Integer.MAX_VALUE;
+            int cnt = 0;
+            for(int i=1;i<=n;i++){
+                if(score[i] < min){
+                    cnt++;
+                    min = score[i];
                 }
-            });
-            PriorityQueue<Integer> pq = new PriorityQueue<>();
-            pq.add(student[0][1]);
-            int cnt = 1;
-            for(int i=1;i<n;i++){
-                cnt++;
-                if(student[i][1] > pq.peek()){
-                    cnt--;
-                }
-                pq.add(student[i][1]);
-
             }
             System.out.println(cnt);
         }
