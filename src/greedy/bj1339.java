44ae63d1b50ba1d4818b612be8b11b4c142984f3
Junyoung Choi <cjy1705@naver.com>
2020-10-12 21:41:52 +0900

    greedy

---
+++ src/greedy/bj1339.java
@@ -0,0 +1,17 @@
+package greedy;
+import java.util.*;
+public class bj1339 {
+    static Scanner sc = new Scanner(System.in);
+    static int n;
+    public static void main(String[] args){
+        n = sc.nextInt();
+
+        int k=9;
+        int max = 0;
+        String[] s = new String[n];
+        for(int i=0;i<n;i++){
+
+
+        }
+    }
+}
